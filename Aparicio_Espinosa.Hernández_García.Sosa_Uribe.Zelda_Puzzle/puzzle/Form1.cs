5a605c9b8b277a35a951d167c2525e2b6e1b04d0
SebastianBB <38845144+SebastianBB@users.noreply.github.com>
2018-04-30 21:01:59 -0500

    Add files via upload

---
+++ Aparicio_Espinosa.Hernández_García.Sosa_Uribe.Zelda_Puzzle/puzzle/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmPuzzleGame : Form
+    {
+        int inNullSliceIndex, inmoves = 0;
+        List<Bitmap> lstOriginalPictureList = new List<Bitmap>();
+        System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+
+        public frmPuzzleGame()
+        {
+            InitializeComponent();
+            lstOriginalPictureList.AddRange(new Bitmap[] { Properties.Resources._1, Properties.Resources._2,
+                Properties.Resources._3, Properties.Resources._4, Properties.Resources._5, Properties.Resources._6,
+                Properties.Resources._7, Properties.Resources._8, Properties.Resources._9, Properties.Resources._null });
+            lblMovesMade.Text += inmoves;
+            lblTimeElapsed.Text = "00:00:00";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Shuffle();
+        }
+
+        void Shuffle()
+        {
+            do
+            {
+                int j;
+                List<int> Indexes = new List<int>(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 9 });//8 no está presente porque es la última imagen
+                Random r = new Random();
+                for (int i = 0; i < 9; i++)
+                {
+                    Indexes.Remove((j = Indexes[r.Next(0, Indexes.Count)]));
+                    ((PictureBox)gbPuzzleBox.Controls[i]).Image = lstOriginalPictureList[j];
+                    if (j == 9) inNullSliceIndex = i;
+                }
+            } while (CheckWin());
+        }
+
+        private void btnShuffle_Click(object sender, EventArgs e)
+        {
+            DialogResult YesOrNo = new DialogResult();     
+            if (lblTimeElapsed.Text != "00:00:00")
+            {
+                YesOrNo = MessageBox.Show("¿Estás seguro que deseas REINICIAR?", "Zelda Puzzle", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            }
+            if (YesOrNo == DialogResult.Yes || lblTimeElapsed.Text == "00:00:00")
+            {
+                Shuffle();
+                timer.Reset();
+                lblTimeElapsed.Text = "00:00:00";
+                inmoves = 0;
+                lblMovesMade.Text = "Movimientos Hechos : 0";
+            }
+        }
+
+        private void AskPermissionBeforeQuite(object sender, FormClosingEventArgs e)
+        {
+            DialogResult YesOrNO = MessageBox.Show("¿Estás seguro que deseas SALIR?", "Zelda Puzzle", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (sender as Button != btnQuit && YesOrNO == DialogResult.No) e.Cancel = true;
+            if (sender as Button == btnQuit && YesOrNO == DialogResult.Yes) Environment.Exit(0);
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            AskPermissionBeforeQuite(sender, e as FormClosingEventArgs);
+        }
+
+        private void SwitchPictureBox(object sender, EventArgs e)
+        {
+            if (lblTimeElapsed.Text == "00:00:00")
+                timer.Start();
+            int inPictureBoxIndex = gbPuzzleBox.Controls.IndexOf(sender as Control);
+            if (inNullSliceIndex != inPictureBoxIndex)
+            {
+                List<int> FourBrothers = new List<int>(new int[] { ((inPictureBoxIndex % 3 == 0) ? -1 : inPictureBoxIndex - 1),
+                    inPictureBoxIndex - 3, (inPictureBoxIndex % 3 == 2) ? -1 : inPictureBoxIndex + 1, inPictureBoxIndex + 3 });
+                if (FourBrothers.Contains(inNullSliceIndex))
+                {
+                    ((PictureBox)gbPuzzleBox.Controls[inNullSliceIndex]).Image = ((PictureBox)gbPuzzleBox.Controls[inPictureBoxIndex]).Image;
+                    ((PictureBox)gbPuzzleBox.Controls[inPictureBoxIndex]).Image = lstOriginalPictureList[9];
+                    inNullSliceIndex = inPictureBoxIndex;
+                    lblMovesMade.Text = "Movimientos Hechos : " + (++inmoves);
+                    if (CheckWin())
+                    {
+                        timer.Stop();
+                        (gbPuzzleBox.Controls[8] as PictureBox).Image = lstOriginalPictureList[8];
+                        MessageBox.Show("Felicitaciones...\nHas completado el Puzzle\nTiempo transcurrido : " + timer.Elapsed.ToString().Remove(8) + "\nTotal de movimientos : " + inmoves, "Zelda Puzzle");
+                        inmoves = 0;
+                        lblMovesMade.Text = "Movimientos Hechos : 0";
+                        lblTimeElapsed.Text = "00:00:00";
+                        timer.Reset();
+                        Shuffle();
+                    }
+                }
+            }
+        }
+
+        bool CheckWin()
+        {
+            int i;
+            for (i = 0; i < 8; i++)
+            {
+                if ((gbPuzzleBox.Controls[i] as PictureBox).Image != lstOriginalPictureList[i]) break;
+            }
+            if (i == 8) return true;
+            else return false;
+        }
+
+        private void UpdateTimeElapsed(object sender, EventArgs e)
+        {
+            if (timer.Elapsed.ToString() != "00:00:00")
+                lblTimeElapsed.Text = timer.Elapsed.ToString().Remove(8);
+            if (timer.Elapsed.ToString() == "00:00:00")
+                btnPause.Enabled = false;
+            else
+                btnPause.Enabled = true;
+            if (timer.Elapsed.Minutes.ToString() == "1")
+            {
+                timer.Reset();
+                lblMovesMade.Text = "Movimientos Hechos : 0";
+                lblTimeElapsed.Text = "00:00:00";
+                inmoves = 0;
+                btnPause.Enabled = false;
+                MessageBox.Show("Se acabó el tiempo\nIntenta de nuevo", "Zelda Puzzle");
+                Shuffle();
+            }
+        }
+
+        private void PauseOrResume(object sender, EventArgs e)
+        {
+            if (btnPause.Text == "Pausar")
+            {
+                timer.Stop();
+                gbPuzzleBox.Visible = false;
+                btnPause.Text = "Resumir";
+            }
+            else
+            {
+                timer.Start();
+                gbPuzzleBox.Visible = true;
+                btnPause.Text = "Pausar";
+            }
+        }
+    }
+}
